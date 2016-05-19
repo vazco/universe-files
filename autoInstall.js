a71d671c35939d990e6c9f03bcc076a0ade3e63e
Cristo <cristo.rabani@gmail.com>
2016-05-20 00:19:15 +0200

    Update autoInstall.js

--- autoInstall.js
+++ autoInstall.js
@@ -3,6 +3,5 @@ import autoInstallNpm from 'meteor/cristo:auto-install-npm';
 autoInstallNpm({
     uploadfs: '^1.3.1',
     mkdirp: '^0.3.4',
-    'upload-file': '^1.2.0',
-    'jquery-simple-upload': '^1.0.0'
+    'upload-file': '^1.2.0'
 }, 'files');
