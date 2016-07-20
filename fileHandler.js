2ea16a4a5aaffb0f76491c87ba52b97cef65a6d8
Mariusz Przodała <mariusz.przodala@proserwit.pl>
2016-07-20 11:17:41 +0200

    download file as attachment with file name change

--- fileHandler.js
+++ fileHandler.js
@@ -6,6 +6,7 @@ import {UPLOADS_URL, UPLOADS_PATH} from './constants';
 
 if (UPLOADS_URL) {
     WebApp.connectHandlers.use(UPLOADS_URL, function (req, res, next) {
+        const {fileName, download} = req.query;
         const url = (req.url || '').replace(/\?.*$/, '');
         const localPath = path.join(UPLOADS_PATH, url.replace(/\.\.|~|^\\+|^\/+/g, ''));
         if (!fs.existsSync(localPath)) {
@@ -14,6 +15,25 @@ if (UPLOADS_URL) {
         const stat = fs.statSync(localPath);
         let ext = path.extname(localPath) || '';
         ext = ext.replace(/^\./, '');
+
+        if(download){
+            if(fileName) {
+                const downloadFileName = fileName.replace(/ą/g, 'a').replace(/Ą/g, 'A')
+                                                 .replace(/ć/g, 'c').replace(/Ć/g, 'C')
+                                                 .replace(/ę/g, 'e').replace(/Ę/g, 'E')
+                                                 .replace(/ł/g, 'l').replace(/Ł/g, 'L')
+                                                 .replace(/ń/g, 'n').replace(/Ń/g, 'N')
+                                                 .replace(/ó/g, 'o').replace(/Ó/g, 'O')
+                                                 .replace(/ś/g, 's').replace(/Ś/g, 'S')
+                                                 .replace(/ż/g, 'z').replace(/Ż/g, 'Z')
+                                                 .replace(/ź/g, 'z').replace(/Ź/g, 'Z') + '.' + ext;
+                res.setHeader('Content-disposition', 'attachment; filename=' + downloadFileName);
+            }else{
+                res.setHeader('Content-disposition', 'attachment');
+            }
+            res.setHeader('Content-Description', 'File Transfer');
+        }
+
         res.writeHead(200, {
             'Content-Type': contentTypes[ext] || 'application/octet-stream',
             'Content-Length': stat.size
